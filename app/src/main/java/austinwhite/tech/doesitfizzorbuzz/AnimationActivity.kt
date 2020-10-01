63197ccf73d906aed6eb4b8ea634f71181a5d39f
endlessrequiem <austin.j.white.96@gmail.com>
2020-10-01 14:35:10 -0700

    deleted comments

--- app/src/main/java/austinwhite/tech/doesitfizzorbuzz/AnimationActivity.kt
+++ app/src/main/java/austinwhite/tech/doesitfizzorbuzz/AnimationActivity.kt
@@ -34,9 +34,6 @@ class AnimationActivity : AppCompatActivity() {
 
     private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
 
-        // This extension method listens for start/end events on an animation and disables
-        // the given view for the entirety of that animation.
-
         addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationStart(animation: Animator?) {
                 view.isEnabled = false
